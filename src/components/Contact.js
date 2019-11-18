fdd42a644350bf12dd6c52edbdf21ed6c411aebb
rrgallop <ryan.gallop@maine.edu>
2019-11-18 00:04:10 -0500

    Laying groundwork for contact me form zzz

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,34 +2,51 @@ import React from 'react';
 import '../style.css';
 
 
-const Home = () => {
-    return(
-        <div>
-        <section className="aboutme">
-            <section className="tagline">
-                <h2>Professional Software Engineer</h2>
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+    }
+    
+
+    handleSubmit = (event) => {
+        alert('The button doesn\'t actually work yet, but thank you for your interest! Please reach out to me directly at rrgallop@gmail.com. I look forward to hearing from you.');
+        event.preventDefault();
+    }
+
+    handleChange = (event) =>{
+        this.setState({value: event.target.value})
+    }
+
+    render(){
+        return(
+            <div>
+            <section className="aboutme">
+                <section className="tagline">
+                    <h2>Professional Software Engineer</h2>
+                </section>
+                <section className="infoblurb">
+                    <h3>I love to code. I love to learn.<br/>Working with tech is my passion.<br/>
+                        If it's yours too, I would love to be a part<br/>
+                        of your organization.
+                    </h3>
+                </section>
             </section>
-            <section className="infoblurb">
-                <h3>I love to code. I love to learn.<br/>Working with tech is my passion.<br/>
-                    If it's yours too, I would love to be a part<br/>
-                    of your organization.
-                </h3>
-            </section>
-        </section>
-    <section className="contactme">
-        <div className="container">
-            <h4>If you like what you see and would like to get in contact,<br/>
-                 just enter your email address below.<br/>
-                 I will get back to you as soon as I can.
-            </h4>
-            <form>
-                <input type="email" placeholder="you@email.com"/>
-                <button type="submit" className="button_1">Submit</button>
-            </form>
-        </div>
-    </section>
-        </div>
-    );
+                <section className="contactme">
+                    <div className="container">
+                        <h4>If you like what you see and would like to get in contact,<br/>
+                            just enter your email address below.<br/>
+                            I will get back to you as soon as I can.
+                        </h4>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="email" placeholder="you@email.com" value={this.state.value} onChange={this.handleChange} />
+                            <button type="submit" className="button_1">Submit</button>
+                        </form>
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Home;
